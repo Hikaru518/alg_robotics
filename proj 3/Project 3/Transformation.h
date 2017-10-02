bf53b0c3012370dd5bb989b54d697e434a2c5d7a
sichaozhang1112 <schizoganah@gmail.com>
2017-10-02 00:26:39 -0500

    upload RTP demo

---
+++ proj 3/Project 3/Transformation.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+
+// Rotate counterclockwise around a point. Point p is rotating around pivot point by theta.
+
+// Return the coordinates of the point
+double getRotatedPoint_X(double px, double py, double pivotX, double pivotY, double theta);
+double getRotatedPoint_Y(double px, double py, double pivotX, double pivotY, double theta);
