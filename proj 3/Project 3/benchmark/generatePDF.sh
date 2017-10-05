33bfed29000f0f5330bde173731b67c4fbfea7fe
chi1shi2 <wpg@bupt.edu.cn>
2017-10-05 00:03:16 -0500

    benchmark result pdf

--- proj 3/Project 3/benchmark/generatePDF.sh
+++ proj 3/Project 3/benchmark/generatePDF.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # This script is to generate result pdf files for benchmark data.
 
-./ompl_benchmark_statistics.py ./data/uniform_sampler.log -d ./data/uniform_sampler.db
-./ompl_benchmark_statistics.py -d ./data/uniform_sampler.db -p ./results_pdf/uniform_sampler_results.pdf
+./ompl_benchmark_statistics.py ./data/C_uniform_sampler.log -d ./data/C_uniform_sampler.db
+./ompl_benchmark_statistics.py -d ./data/C_uniform_sampler.db -p ./results_pdf/C_uniform_sampler_results.pdf
 
-./ompl_benchmark_statistics.py ./data/gaussian_sampler.log -d ./data/gaussian_sampler.db
-./ompl_benchmark_statistics.py -d ./data/gaussian_sampler.db -p ./results_pdf/gaussian_sampler.pdf
+./ompl_benchmark_statistics.py ./data/T_uniform_sampler.log -d ./data/T_uniform_sampler.db
+./ompl_benchmark_statistics.py -d ./data/T_uniform_sampler.db -p ./results_pdf/T_uniform_sampler_results.pdf
 
-./ompl_benchmark_statistics.py ./data/obstaclebased_sampler.log -d ./data/obstaclebased_sampler.db
-./ompl_benchmark_statistics.py -d ./data/obstaclebased_sampler.db -p ./results_pdf/obstaclebased_sampler.pdf
-
-./ompl_benchmark_statistics.py ./data/maxclearance_sampler.log -d ./data/maxclearance_sampler.db
-./ompl_benchmark_statistics.py -d ./data/maxclearance_sampler.db -p ./results_pdf/maxclearance_sampler.pdf
+rm *.log
+rm *.console
