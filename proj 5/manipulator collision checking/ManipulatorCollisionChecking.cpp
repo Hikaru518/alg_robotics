c9f37e82a65feb3acd9e10da47e61338ebfe55c7
sichaozhang1112 <schizoganah@gmail.com>
2017-11-22 11:10:30 -0600

    collision checking again

--- proj 5/manipulator collision checking/ManipulatorCollisionChecking.cpp
+++ proj 5/manipulator collision checking/ManipulatorCollisionChecking.cpp
@@ -1,26 +1,35 @@
-//#include "ManipulatorCollisionChecking.h"
+#include "ManipulatorCollisionChecking.h"
 #include "isLineIntersect.h"
 
 #include <vector>
 #include <cmath>
 #include <iostream>
 
-typedef std::pair<double,double> Point2D;
-typedef std::vector<Point2D> Rectangle;
 
-bool isLinePoint(
-    const std::pair<double, double>& line_start, 
-    const std::pair<double, double>& line_end,
-    double ob_x, double ob_y, double ob_wid, double ob_hei)
+void getObstacles(std::vector<Rectangle>& obstacles)
 {
+    Rectangle rect;
+    rect.x = -1.0;
+    rect.y = 3.0;
+    rect.height = 1.0;
+    rect.width = 2.0;
+    obstacles.push_back(rect);
+}
+
+bool isValidLine(const std::pair<double, double>& line_start, 
+                 const std::pair<double, double>& line_end,
+                 const std::vector<Rectangle>& obstacles)
+{
+  for (int i = 0; i<obstacles.size(); i++)
+  {
     double line_x1 = line_start.first;
     double line_y1 = line_start.second;
     double line_x2 = line_end.first;
     double line_y2 = line_end.second;
-    double ob_x = -1;
-    double ob_y = 3;
-    double ob_wid = 2;
-    double ob_hei = 1;
+    double ob_x = obstacles[i].x;
+    double ob_y = obstacles[i].y;
+    double ob_wid = obstacles[i].width;
+    double ob_hei = obstacles[i].height;
     pts[0] = std:pair<line_x1,line_y1>;
     pts[1] = std:pair<line_x2,line_y2>;
     pts[2] = std:pair<ob_x,ob_y>;
@@ -35,6 +44,7 @@ bool isLinePoint(
     {
         return false;
     }
+  }
   return true;
 }
 
